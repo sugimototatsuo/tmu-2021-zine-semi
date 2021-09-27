e8eda6d0f3bb41353d70810fd27032eb014a1814
SUGIMOTO Tatsuo <sugi@zero-out.com>
2021-09-27 14:10:12 +0900

    rm shoelace component

--- docs/index.js
+++ docs/index.js
@@ -82,42 +82,6 @@ async function loadData() {
     .on("click", (e) => {
       openLightbox(e);
     });
-
-  // cardFooter.append('span')
-  //   .classed('card-footer-item', true)
-  //   .text('test')
-
-  // const zine = d3
-  //   .select("#zine-list")
-  //   .selectAll("sl-card")
-  //   .data(data)
-  //   .join("sl-card")
-  //   .classed("zine-overview", true)
-  //   .classed("card", true);
-  // zine
-  //   // .append("a")
-  //   // .attr("href", (d, i) => `zine/${d3.format("02d")(i + 1)}/01.jpg`)
-  //   .append("img")
-  //   .attr("src", (d, i) => `images/${d3.format("02d")(i + 1)}.jpg`)
-  //   .attr("alt", (d) => d.title)
-  //   .attr("slot", "image")
-  //   .on("click", (e) => {
-  //     openLightbox(e);
-  //   })
-  //   .style('cursor', 'pointer');
-  // zine.append("strong").text((d) => d.title);
-  // zine.append("p").text((d) => d.name);
-  // zine.append("small").text((d) => d.description);
-  // const footer = zine.append("div").attr("slot", "footer");
-
-  // footer
-  //   .append("sl-button")
-  //   .attr("type", "primary")
-  //   .attr("pill", "")
-  //   .html(`<sl-icon name="book"></sl-icon> 読む`)
-  //   .on("click", (e) => {
-  //     openLightbox(e);
-  //   });
 }
 
 loadData();
